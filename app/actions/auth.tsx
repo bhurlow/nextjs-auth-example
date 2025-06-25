dc04ea6a9554ba2cb46b79a8783f9499b766c5a5
Brian Hurlow <brian@brianhurlow.com>
2025-06-25 14:18:53 -0400

    missing "use server" tag for action

--- app/actions/auth.tsx
+++ app/actions/auth.tsx
@@ -1,6 +1,9 @@
+'use server'
+
 import { createSession } from '@/app/lib/session'
 
 export async function signup(formData: FormData) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   console.log('HELLO AUTH ACTION')
 
   await createSession()
